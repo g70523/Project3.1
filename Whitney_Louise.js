4e6a7f676770c3eb8f03f3f2e75093115b4a0ce9
Louise Whitney <louisewhitney@fullsail.edu>
2013-03-21 17:30:12 -0400

    Added Accessor and Mutator Methods

--- Whitney_Louise.js
+++ Whitney_Louise.js
@@ -18,26 +18,102 @@ var fairies={
             "name":     "LaLa",
             "color":    "green",
             "power":    "shocks",
-            "position": "follower" 
+            "position": "princess" 
         },
         {
-            "name":    "KiKi",
+            "name":     "KiKi",
             "color":    "pink",
             "power":    "stuns",
             "position": "follower"
         },
         {
             "name":     "MiMi",
-            "color":     "blue",
-            "power":     "shrink",
-            "position":  "follower"
+            "color":    "blue",
+            "power":    "shrink",
+            "position": "follower"
         },
         {
             "name":     "FiFi",
-            "color":     "Red",
-            "power":     "fire",
-            "position":  "follower"
+            "color":    "Red",
+            "power":    "fire",
+            "position": "follower"
+        },
+        {
+            "name":    "Mack",
+            "color":   "orange",
+            "power":   "grow",
+            "position":"follower"
         }
     ]
     
     };
+    
+var newFairies={
+    "goodFairy": [
+        {
+            "name":     "Sasha",
+            "color":    "turqoise",
+            "power":    "water",
+            "position": "leader"
+        },
+        {
+            "name":     "Tink",
+            "color":    "fuchsia",
+            "power":    "love",
+            "position": "co-leader" 
+        },
+        {
+            "name":     "Mary",
+            "color":    "violet",
+            "power":    "protection",
+            "position": "follower"
+        },
+        {
+            "name":     "Babs",
+            "color":    "yellow",
+            "power":    "light",
+            "position": "follower"
+        },
+        {
+            "name":     "Paisley",
+            "color":    "tan",
+            "power":    "warmth",
+            "position": "follower"
+        },
+        {
+            "name":    "KayKay",
+            "color":   "maroon",
+            "power":   "chaos",
+            "position":"follower"
+        } 
+    ]
+    };
+    
+//Fairy Objects
+
+var fairy = {
+
+    name:       "Sasha",
+    color:      "turqoise",
+    power:      "water",
+    position:   "leader",
+    leader:     function(){ //Method Accessor
+        var fairyLeader = fairy.name
+        return fairyLeader;
+    },//End leader Method Accessor
+        makeNaughty: function(name,power){ //Method Mutator
+        this.name = name;
+        this.power = power;
+        console.log("Your new name is " + name + " and your new power is to " + power + ".");
+        
+    } //End makeNaughty Method Mutator
+    
+
+};
+ console.log("The leader of the good fairies is " + fairy.leader() + "." + "" + "Her fairy color is " + fairy.color + ".")
+ console.log("And her fairy power is " + fairy.power + ".")
+ fairy.makeNaughty("Sassy", "drown")
+
+
+
+
