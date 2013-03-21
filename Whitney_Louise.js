2782d376f6cfd493b39963bc8a7530ec1ee08775
Louise Whitney <louisewhitney@fullsail.edu>
2013-03-21 15:00:30 -0400

    Added JSON

--- Whitney_Louise.js
+++ Whitney_Louise.js
@@ -1,1 +1,43 @@
-alert("JavaScript works!");
+/*Louise Whitney
+ SDI 1303
+ Project 3
+ Fairies!
+ */
+
+//JSON
+
+var fairies={
+    "naughtyFairy": [
+        {
+            "name":     "LuLu",
+            "color":    "purple",
+            "power":    "wind",
+            "position": "queen"
+        },
+        {
+            "name":     "LaLa",
+            "color":    "green",
+            "power":    "shocks",
+            "position": "follower" 
+        },
+        {
+            "name":    "KiKi",
+            "color":    "pink",
+            "power":    "stuns",
+            "position": "follower"
+        },
+        {
+            "name":     "MiMi",
+            "color":     "blue",
+            "power":     "shrink",
+            "position":  "follower"
+        },
+        {
+            "name":     "FiFi",
+            "color":     "Red",
+            "power":     "fire",
+            "position":  "follower"
+        }
+    ]
+    
+    };
